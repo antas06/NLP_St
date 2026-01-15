91966c882351c8addf63da5706f7d254bb53e401
antas06 <antas.hft@gmail.com>
2026-01-15 22:21:44 +0530

    Updated the scripts

--- data_aqusation/Movie_rating_api.py
+++ data_aqusation/Movie_rating_api.py
@@ -1,10 +1,8 @@
 import requests
 import json
 
-url = "https://api.themoviedb.org/3/genre/movie/list?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
-
-response = requests.get(url)
+response =requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US&page=1")
 data = response.json()
 
-with open("NLP_St\data\Movie_Ratings\genres_id.json", "w") as f:
+with open ("NLP_St\data\Movie_Ratings\movie_review_all.json", "w") as f:
   json.dump(data, f, indent=4)
