50d5331b02acfa4673ca2c41cd3163226b63405a
antas06 <antas.hft@gmail.com>
2026-01-17 21:39:36 +0530

    Cleansing done

--- Text_Preprocessing/Movie_review_preprocessing.py
+++ Text_Preprocessing/Movie_review_preprocessing.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 #complete Dataframe
-com_df= pd.read_json("NLP_St\data\Movie_Ratings\movie_reviews.json")
+com_df= pd.read_json(r"NLP_St\data\Movie_Ratings\movie_reviews.json")
 
 #Dataframe that will be used
 df_use = com_df[["genre_ids", "overview", "title"]]
@@ -11,4 +11,4 @@ df_use = com_df[["genre_ids", "overview", "title"]]
 gid_df = pd.read_json("NLP_St\data\Movie_Ratings\genres_id.json")
 gid_list = list(gid_df["id"])
 
-print(df_use["overview"][4])
+df_use.to_csv(r'NLP_St\data\Movie_Ratings\df_use_csv.csv', index=False)
