0feaad40f79916408f246849a08db4233861548e
antas06 <antas.hft@gmail.com>
2026-02-09 19:34:28 +0530

    Word2Vec

--- Word2Vec/Preprop_and_w2v_training.py
+++ Word2Vec/Preprop_and_w2v_training.py
@@ -21,8 +21,6 @@ import nltk
 from nltk import sent_tokenize
 from gensim.utils import simple_preprocess
 
-from nltk.corpus import stopwords
-
 # Download stopwords
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -43,8 +41,44 @@ for filename in os.listdir(path):
       documents.append(simple_preprocess(sent))
 
 print("Sentences tokenised and simple preprocessed each sentence.")
-print("Sample: ")
-print(documents[0])
+print("Sample: len of a doc")
+print(len(documents[98]))
 print("---------------------------------------------------------------------------------")
 
 #start from removing stopwods in each document(sentence).
+new_documents = []
+for doc in documents:
+  new_doc = [word for word in doc if word not in stop_words]
+  new_documents.append(new_doc)
+print("Stopword Removed.")
+print("Sample len of same doc:")    
+print(len(new_documents[98]))
+print("--------------------------------------------------------------------------------")
+
+#Word2Vec model initialized
+model = gensim.models.Word2Vec(
+    window=10,
+    min_count=2
+)
+
+#building vocab for custom model
+model.build_vocab(new_documents)
+
+#training the model
+model.train(new_documents, total_examples=model.corpus_count, epochs=model.epochs)
+
+#Model is now trained and ready for use with it's function
+
+print("using functions of a Word2Vec model")
+print("Most similar to places: ")
+model.wv.most_similar("places")
+
+#embeddings of the words
+#by default size =100
+model.wv["king"]
+
+mo = model.wv.get_normed_vectors()
+print(mo.shape)
+
+print("Similaity b/w King and Queen through our embeddings: ")
+print(model.wv.similarity("king", "queen"))
